30722379df9a377559ae5ea9bdb98adc5cece60d
Alexandr Borecky <alexandrborecky@MacBook-Pro-2.local>
2022-10-29 22:37:20 +0200

    Text changes

--- pages/index.vue
+++ pages/index.vue
@@ -13,8 +13,7 @@
           <div class="title content flex column half-width">
             <h5>Bio</h5>
             <h1>Alena Doláková</h1>
-            <p>The star of <a href="https://www.simonholy.com/index.php/zrcadla-ve-tme/?fbclid=IwAR0gv0-rYXVDsVIlbAbkTzpXO-ltZVTds0NEflPqE6mcq1LG1J2of52kb58" target="%blank"> <div class="underline"></div> Mirrors in The Dark</a> which premiered at KVIFF also appeared in six US series (playing an American, a Brit, a Bulgarian etc.) and other thirty Czech projects. Alena is also a published author (<a href="https://www.albatrosmedia.cz/tituly/63735820/anna-z-hollywoodu/?fbclid=IwAR2WU7x-pabNMLHF3-8R76mEsHEmBXGvAeA3lmYYOtZ3_2BETrf_Qa8mJFk" target="%blank"> <div class="underline"></div> Anna Of Hollywood,</a> Albatros Media), owner of <a href="https://annaent.com/" target="%blank"> <div class="underline"></div> ANNA Entertainment</a> and activist.
-              Alena Doláková je česká herečka s mezinárodní zkušeností. V listopadu 2020 debutovala knihou  (Albatros Media), věnuje se také stand upu a píše fejetony (Glanc, Elle). Letos se představí v hlavní roli ve filmu Zrcadla ve tmě.</p>
+            <p>The star of <a href="https://www.simonholy.com/index.php/zrcadla-ve-tme/?fbclid=IwAR0gv0-rYXVDsVIlbAbkTzpXO-ltZVTds0NEflPqE6mcq1LG1J2of52kb58" target="%blank"> <div class="underline"></div> Mirrors in The Dark</a> which premiered at KVIFF also appeared in six US series (playing an American, a Brit, a Bulgarian etc.) and other thirty Czech projects. Alena is also a published author (<a href="https://www.albatrosmedia.cz/tituly/63735820/anna-z-hollywoodu/?fbclid=IwAR2WU7x-pabNMLHF3-8R76mEsHEmBXGvAeA3lmYYOtZ3_2BETrf_Qa8mJFk" target="%blank"> <div class="underline"></div> Anna Of Hollywood,</a> Albatros Media), owner of <a href="https://annaent.com/" target="%blank"> <div class="underline"></div> ANNA Entertainment</a> and activist.</p>
             <div class="links flex">
               <ul>
                 <li><a class="flex middle" target="_blank" href="https://www.imdb.com/name/nm5794772/"><div class="underline"></div> IMDB <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a></li>
@@ -26,14 +25,14 @@
         <div class="projects hero flex page-section">
           <div class="title flex half-width">
             <div class="content flex column">
-              <h5>Aktuálně</h5>
-              <h2>Projekty</h2>
+              <h5>Current</h5>
+              <h2>Projects</h2>
             </div>
           </div>
           <div class="content flex column half-width">
             <ul>
-              <li class="flex"><a href="https://www.albatrosmedia.cz/tituly/63735820/anna-z-hollywoodu/?fbclid=IwAR2WU7x-pabNMLHF3-8R76mEsHEmBXGvAeA3lmYYOtZ3_2BETrf_Qa8mJFk" target="_blank"> <div class="underline"></div> Anna z Hollywoodu</a><p>kniha</p></li>
-              <li class="flex"><a href="https://annaent.com/" target="_blank"> <div class="underline"></div> Anna Entertainment</a><p>kniha</p></li>
+              <li class="flex"><a href="https://www.albatrosmedia.cz/tituly/63735820/anna-z-hollywoodu/?fbclid=IwAR2WU7x-pabNMLHF3-8R76mEsHEmBXGvAeA3lmYYOtZ3_2BETrf_Qa8mJFk" target="_blank"> <div class="underline"></div> Anna z Hollywoodu</a><p>book</p></li>
+              <li class="flex"><a href="https://annaent.com/" target="_blank"> <div class="underline"></div> Anna Entertainment</a><p>co</p></li>
               <li class="flex"><a href="https://www.simonholy.com/index.php/zrcadla-ve-tme/?fbclid=IwAR0gv0-rYXVDsVIlbAbkTzpXO-ltZVTds0NEflPqE6mcq1LG1J2of52kb58" target="_blank"><div class="underline"></div>Zrcadla ve tmě</a><p>film</p></li>
               <!-- <li class="flex"><a href="https://chcitopodcast.cz" target="_blank"><div class="underline"></div>Chci To</a><p>podcast</p></li> -->
               <li class="flex"><a href="https://421686.myshoptet.com/" target="_blank"> <div class="underline"></div>Shop</a><p>merch</p></li>
@@ -44,7 +43,7 @@
           <div class="title flex half-width">
             <div class="content flex column">
               <h5>Get in touch</h5>
-              <h2>Kontakty</h2>
+              <h2>Contacts</h2>
             </div>
           </div>
           <div class="contact-info half-width">
